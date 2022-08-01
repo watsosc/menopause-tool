c36591c31df7dfcc95c79f3f7d3b3287bf671527
watsosc <watsosc@protonmail.com>
2022-08-01 12:18:54 -0400

    Add medical history card

---
+++ src/routes/results/MedicalHistory.svelte
@@ -0,0 +1,46 @@
+<script lang="ts">
+	import { Card, Content } from '$lib/layouts';
+	import { history } from '../../store';
+	import { getDescription, historyOptions } from '$lib/selections';
+	import SummaryTitleBar from '$lib/titles/SummaryTitleBar.svelte';
+</script>
+
+<div class="container mx-auto px-4">
+	<Card>
+		<SummaryTitleBar>Medications</SummaryTitleBar>
+		<Content>
+			{#if $history.conditions.length > 0}
+				<p class="font-body text-xl">
+					From our predetermined list, you indicated you have/had the following medical conditions
+					now or in the past:
+				</p>
+				<ul class="list-disc ml-10">
+					{#each $history.conditions as condition}
+						<li class="font-body text-xl">
+							{getDescription(historyOptions.conditions, condition)}
+						</li>
+					{/each}
+				</ul>
+			{:else}
+				<p class="font-body text-xl">
+					You indicated you <b>have not had</b> any of the following medical conditions now or in the
+					past:
+				</p>
+				<ul class="list-disc ml-10">
+					{#each historyOptions.conditions as condition}
+						<li class="font-body text-xl">{condition.description}</li>
+					{/each}
+				</ul>
+			{/if}
+			<p class="font-body text-xl">
+				{#if $history.bloodPressure == 'uncontrolled'}
+					You have high blood pressure but it is not controlled.
+				{:else if $history.bloodPressure == 'controlled'}
+					You have a high blood pressure but it is controlled.
+				{:else}
+					You have no history of high blood pressure.
+				{/if}
+			</p>
+		</Content>
+	</Card>
+</div>
