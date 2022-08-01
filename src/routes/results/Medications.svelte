f5311e67ec8f92dc7e01f953f581fbb850c4df31
watsosc <watsosc@protonmail.com>
2022-08-01 12:11:51 -0400

    Add medications card

---
+++ src/routes/results/Medications.svelte
@@ -0,0 +1,45 @@
+<script lang="ts">
+	import { Card, Content } from '$lib/layouts';
+	import { medication } from '../../store';
+	import { getDescription, medicationOptions } from '$lib/selections';
+	import SummaryTitleBar from '$lib/titles/SummaryTitleBar.svelte';
+</script>
+
+<div class="container mx-auto px-4">
+	<Card>
+		<SummaryTitleBar>Medications</SummaryTitleBar>
+		<Content>
+			<p class="font-body text-xl">
+				You were given a list of different types of medications. From this list, you indicated you
+				are taking the following medications/types of medications:
+			</p>
+			{#if $medication.medicationSelection.length > 0}
+				<ul class="list-disc ml-10">
+					{#each $medication.medicationSelection as med}
+						<li class="font-body text-xl">{getDescription(medicationOptions.medications, med)}</li>
+					{/each}
+				</ul>
+			{:else}
+				<ul class="list-disc ml-10">
+					<li class="font-body text-xl">None</li>
+				</ul>
+			{/if}
+			<p class="font-body text-xl">
+				{#if !!$medication.medicationEntry}
+					You were then asked to list all medications you take on a regular basis. You indicated you
+					take the following medications: {$medication.medicationEntry}
+				{:else}
+					You were asked to list all medications you take on a regular basis. You indicated you take
+					no medication at the moment.
+				{/if}
+			</p>
+			<p class="font-body text-xl">
+				{#if !!$medication.allergiesText}
+					Regarding allergies, you have indicated the following allergies: {$medication.allergiesText}
+				{:else}
+					Regarding allergies, you have indicated you have <b>no</b> allergies.
+				{/if}
+			</p>
+		</Content>
+	</Card>
+</div>
