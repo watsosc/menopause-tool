c039a5a24b226dca7b67bdbdba2ef6fd04f63f24
watsosc <watsosc@protonmail.com>
2022-08-29 20:58:58 -0400

    Make none checkboxes unselectable

--- src/lib/questions/MultiSelectQuestion.svelte
+++ src/lib/questions/MultiSelectQuestion.svelte
@@ -20,7 +20,9 @@
 		default:
 			'w-5 h-5 mx-2 mt-1 rounded border-primary border-2 text-title focus:ring-2 focus:ring-title disabled:border-grey disabled:text-white',
 		error:
-			'w-5 h-5 mx-2 mt-1 rounded border-error border-2 text-title focus:ring-2 focus:ring-title'
+			'w-5 h-5 mx-2 mt-1 rounded border-error border-2 text-title focus:ring-2 focus:ring-title',
+		noneClass:
+			'w-5 h-5 mx-2 mt-1 rounded border-primary border-2 text-title focus:ring-2 focus:ring-title'
 	};
 
 	const handleBlur = () => {
@@ -31,38 +33,55 @@
 <div class="flex flex-col mb-4">
 	<QuestionLabel {name} label={title} {subtitle} />
 	{#each options as option, i (option.id)}
-		<div class="flex flex-row items-start mt-1">
-			<input
-				class={inputClasses['default']}
-				type="checkbox"
-				id={`${name}-${i}`}
-				{name}
-				value={option.id}
-				bind:group={selection}
-			/>
-			<label class="text-lg font-body ml-2 leading-snug" for={`${name}-${i}`}>
-				{@html option.description}
-			</label>
-		</div>
-		{#if option.suboptions && selected.includes(option.id)}
-			{#each option.suboptions as suboption, j (suboption.id)}
-				<div class="ml-2">
-					<div class="flex flex-row items-start mt-1">
-						<input
-							class={inputClasses[isError && errorField === option.id ? 'error' : 'default']}
-							type="checkbox"
-							id={`${name}-${i}-${j}`}
-							{name}
-							value={suboption.id}
-							bind:group={selection}
-							on:blur|preventDefault={handleBlur}
-						/>
-						<label class="text-lg font-body ml-2 leading-snug" for={`${name}-${i}-${j}`}>
-							{@html suboption.description}
-						</label>
+		{#if option.id === 'none'}
+			<div class="flex flex-row items-start mt-1">
+				<input
+					class={inputClasses['noneClass']}
+					type="checkbox"
+					id={`${name}-${i}`}
+					{name}
+					value={option.id}
+					checked={selection.length === 0}
+					disabled={true}
+				/>
+				<label class="text-lg font-body ml-2 leading-snug" for={`${name}-${i}`}>
+					{@html option.description}
+				</label>
+			</div>
+		{:else}
+			<div class="flex flex-row items-start mt-1">
+				<input
+					class={inputClasses['default']}
+					type="checkbox"
+					id={`${name}-${i}`}
+					{name}
+					value={option.id}
+					bind:group={selection}
+				/>
+				<label class="text-lg font-body ml-2 leading-snug" for={`${name}-${i}`}>
+					{@html option.description}
+				</label>
+			</div>
+			{#if option.suboptions && selected.includes(option.id)}
+				{#each option.suboptions as suboption, j (suboption.id)}
+					<div class="ml-2">
+						<div class="flex flex-row items-start mt-1">
+							<input
+								class={inputClasses[isError && errorField === option.id ? 'error' : 'default']}
+								type="checkbox"
+								id={`${name}-${i}-${j}`}
+								{name}
+								value={suboption.id}
+								bind:group={selection}
+								on:blur|preventDefault={handleBlur}
+							/>
+							<label class="text-lg font-body ml-2 leading-snug" for={`${name}-${i}-${j}`}>
+								{@html suboption.description}
+							</label>
+						</div>
 					</div>
-				</div>
-			{/each}
+				{/each}
+			{/if}
 		{/if}
 	{/each}
 	{#if isError}
